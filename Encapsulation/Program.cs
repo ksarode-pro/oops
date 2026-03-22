422049b8ade416da639a36283ac8a2a0db1d1141
Kiran Sarode <ksarode.pro@gmail.com>
2026-03-22 22:16:27 +0530

    encapsulation initial commit

---
+++ Encapsulation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Encapsulation
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World");
+        }
+    }
+}
