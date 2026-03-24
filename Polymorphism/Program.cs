af06ad377775921ca8226ff1bc95845d07cfdd35
Kiran Sarode <ksarode.pro@gmail.com>
2026-03-24 12:28:10 +0530

    static polymorphism added

---
+++ Polymorphism/Program.cs
@@ -0,0 +1,42 @@
+﻿/**
+Polymorphism is same method behaves differently based in context
+Two types: 
+1. Runtime / Dynamic
+2. Compile-time / static
+    - same method anme; but different parameters (different datatype or number of params)
+    - 
+*/
+
+namespace Polymorphism
+{
+    class StaticPolymorphism
+    {
+        // Same method name, different parameters
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Add(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        public string Add(string a, string b)  // concatenation
+        {
+            return a + b;
+        }
+
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            StaticPolymorphism staticPolymorphism = new StaticPolymorphism();
+            System.Console.WriteLine(staticPolymorphism.Add(5, 5));
+            System.Console.WriteLine(staticPolymorphism.Add(5, 5, 5));
+            System.Console.WriteLine(staticPolymorphism.Add("5", "5 - This is string concatination of 5 and 5"));
+        }
+    }
+}
