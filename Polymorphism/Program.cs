e5a63a5c987df362022120b17073c21bcf5a38ea
Kiran Sarode <ksarode.pro@gmail.com>
2026-03-24 12:47:12 +0530

    dynamic polymorphism added

--- Polymorphism/Program.cs
+++ Polymorphism/Program.cs
@@ -1,4 +1,9 @@
-﻿/**
+﻿
+
+using System.ComponentModel;
+
+
+/**
 Polymorphism is same method behaves differently based in context
 Two types: 
 1. Runtime / Dynamic
@@ -6,9 +11,9 @@ Two types:
     - same method anme; but different parameters (different datatype or number of params)
     - 
 */
-
 namespace Polymorphism
 {
+    #region Static Polymorphism
     class StaticPolymorphism
     {
         // Same method name, different parameters
@@ -26,8 +31,39 @@ namespace Polymorphism
         {
             return a + b;
         }
-
     }
+    #endregion
+
+    #region Dynamic Polymorphism
+    class DynamicPolymorphism_Base
+    {
+        //1. method signature should be same in both base and subclass. 
+        //Number of parameters, type of each parameter and return type shold be exactly same.   
+        // Else shows compile time error - no suitable method found to override
+
+        //2. virtual keyword is important - 
+        // compile time error : cannot override inherited member, 
+        // because it is not marked virtual, abstract, or override
+        public virtual int Add(int a, int b)
+        {
+            System.Console.WriteLine("Parent class method");
+            return a + b;
+        }
+    }
+
+    class DynamicPolymorphism_Sub : DynamicPolymorphism_Base
+    {
+        //1. override keyword is necessary for polymorphism
+        //without override keyword, it hides base class implementation like new keyword
+        //2. only warning is shown -  hides inherited member method. 
+        // To make the current member override that implementation, add the override keyword. Otherwise add the new keyword.
+        public override int Add(int a, int b)
+        {
+            System.Console.WriteLine("Subclass method");
+            return a - b;
+        }
+    }
+    #endregion
 
     class Program
     {
@@ -37,6 +73,11 @@ namespace Polymorphism
             System.Console.WriteLine(staticPolymorphism.Add(5, 5));
             System.Console.WriteLine(staticPolymorphism.Add(5, 5, 5));
             System.Console.WriteLine(staticPolymorphism.Add("5", "5 - This is string concatination of 5 and 5"));
+
+            DynamicPolymorphism_Base baseObj = new DynamicPolymorphism_Base();
+            System.Console.WriteLine(baseObj.Add(5, 5));
+            baseObj = new DynamicPolymorphism_Sub();
+            System.Console.WriteLine(baseObj.Add(5, 5));
         }
     }
 }
